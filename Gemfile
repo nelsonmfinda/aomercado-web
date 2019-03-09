7b33f5a73d0b660d5b034839e77f998a85015ccc
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-03-09 06:47:23 +0100

    :art: Config and generate devise Models and Views

--- Gemfile
+++ Gemfile
@@ -54,7 +54,6 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
-
 # A library for generating fake data such as names, addresses, and phone numbers.
 gem 'faker'
 # Flexible authentication solution for Rails with Warden
--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,9 @@ module AomercadoWeb
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    # Rails i18n
+    config.i18n.default_locale = :pt
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end

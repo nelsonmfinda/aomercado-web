a5130819680d58bf7e1b271047751eefa6f4d94e
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-03-09 07:48:33 +0100

    Create and config BackOffice namespace

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery
+//= require bootstrap_sb_admin_base_v2
 //= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :backoffice do
+    get 'dashboard/index'
+  end
   devise_for :admins
   devise_for :members
   get 'home/index'

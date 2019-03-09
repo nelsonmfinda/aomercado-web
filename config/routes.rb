e2b5bf6ea9f170709a2a7d94978573740d6274b6
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-03-09 08:21:29 +0100

    :arts: Destroy Home controller and recreate this with Site namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+
+  namespace :site do
+    get 'home/index'
+  end
+
   namespace :backoffice do
     get 'dashboard/index'
   end
+
   devise_for :admins
   devise_for :members
-  get 'home/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'site/home#index'
 end

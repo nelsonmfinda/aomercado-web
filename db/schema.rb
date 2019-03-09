bc0c0692a5479fe4c05df5508140535216acc1cd
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-03-09 07:19:15 +0100

    Create categories database seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_050927) do
+ActiveRecord::Schema.define(version: 2019_03_09_055151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,12 +32,23 @@ ActiveRecord::Schema.define(version: 2019_03_09_050927) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "description", limit: 60
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_members_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories =  [ 'Animais e acessórios',
+                'Esportes',
+                'Para sua casa',
+                'Eletrônicos e celulares',
+                'Música e hobbies',
+                'Bebês e crianças',
+                'Moda e beleza',
+                'Veículos e barcos',
+                'Imóveis',
+                'Empregos e negócios' ]
+
+categories.each do |category|
+  Category.find_or_create_by(description: category)
+end

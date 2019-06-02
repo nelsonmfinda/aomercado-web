4c7ae4b04883f5a76388534f167b3f94dd7e7353
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-06-02 08:20:53 +0100

    Add form to create edit category

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -1,5 +1,6 @@
 class Backoffice::CategoriesController < BackofficeController
-  
+  before_action :set_category, only: [:edit, :update]
+
   def index
     @categories = Category.all
   end
@@ -21,11 +22,20 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def update
+    if @category.update(params_category)
+      redirect_to backoffice_categories_path, notice: "Categoria atualizada!"
+    else
+      render :edit
+    end
   end
 
   private
 
-  def params_category
-    params.require(:category).permit(:description)
-  end
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def params_category
+      params.require(:category).permit(:description)
+    end
 end

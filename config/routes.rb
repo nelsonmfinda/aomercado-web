cda29f3d63256758ca2168db2fb3cdfd89676674
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-06-23 13:06:01 -0400

    Skip registrations and create admins index route

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   namespace :backoffice do
     resources :categories, except: [:show, :destroy]
     get 'dashboard', to: 'dashboard#index'
+    get 'admins/index'
   end
 
-  devise_for :admins
+  devise_for :admins, :skip => [:registrations]
   devise_for :members
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

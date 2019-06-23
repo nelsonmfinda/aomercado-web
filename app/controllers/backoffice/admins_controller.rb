409c66c8db528255ca44a8106e4795d9fd45f624
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-06-23 13:24:25 -0400

    Implement admins controller

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -1,4 +1,39 @@
 class Backoffice::AdminsController < BackofficeController
   def index
+    @admin = Admin.all
   end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to backoffice_admins_path, notice: "O administrador (#{@admin.email}) foi cadastrado!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @admin.update(params_admin)
+      redirect_to backoffice_admins_path, notice: "Dados do administrador (#{@admin.email}) atualizados!"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def set_admin
+      @admin = Admin.find(params[:id])
+    end
+
+    def params_admin
+      params.require(:admin).permit(:email, :password, :password_confirmation)
+    end
 end
